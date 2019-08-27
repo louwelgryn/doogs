38a35ddd79644344b93a3f62e125e190cfcfcd31
Antoine Gaillard <antoine@agaillard.fr>
2019-08-27 18:59:50 +0200

    ok for new and create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,33 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+    @project = Project.find(params[:project_id])
+    authorize @task
+    @volunteers = project_volunteers
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @project = Project.find(params[:project_id])
+    @task.project = @project
+    authorize @task
+    if @task.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_volunteers
+    @project.commitments.map do |commitment|
+      commitment.user
+    end
+  end
+
+  def tasks_params
+    params.require(:task).permit(:name, :description, :priority_level, :user_id)
+  end
 end
