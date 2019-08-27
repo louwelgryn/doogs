f476b33ae1fa4b9bdcd120fdc0581b880f83b0a2
Lou Welgryn <lou.welgryn@gmail.com>
2019-08-27 15:55:45 +0200

    fixed fucking seeds

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -3,6 +3,10 @@ class CommitmentsController < ApplicationController
   def update
     @project = Project.find(params[:project_id])
     authorize @project
+    current.user.skills.each do |skill|
+      if skill.name = commitment
+      end
+    end
 
   end
 
