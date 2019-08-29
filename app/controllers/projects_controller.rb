3f97f2d30b8607f5785d6b60b00a58845451271d
Marie Stoppa <marie.stoppa@essec.edu>
2019-08-29 18:06:27 +0200

    fix bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
   def dashboard
     @project = Project.find(params[:id])
     @task = Task.new
+    @volunteers = @project.volunteers
     authorize @project
     @manager = manager?
   end
