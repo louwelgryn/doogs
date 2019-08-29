a443710c072ad4af4f88ff48caf3376f2d5e2a7d
maxence ballande <maxence.ballande.16@neoma-bs.com>
2019-08-29 17:18:08 +0200

    tags working for now

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,13 +8,37 @@ class ProjectsController < ApplicationController
   end
 
   def index
+    @statuses = ApplicationRecord::PROJECT_STATUS
+    @thematics = ApplicationRecord::DEVELOPMENT_GOAL
     @projects = policy_scope(Project)
+
+    if params["search"] && params["search"]["status"]
+      @status = params["search"]["status"]
+      case params["search"]["status"]
+      when "on going"
+        @projects = @projects.where(status: 'on going')
+      when "finished"
+        @projects = @projects.where(status: 'finished')
+      when "pending"
+        @projects = @projects.where(status: 'pending')
+      end
+    end
+
+    if params["search"] && params["search"]["thematics"]
+      @thematics = params["search"]["thematics"]
+      ApplicationRecord::DEVELOPMENT_GOAL.each do |thematic|
+        @projects = @projects.where(development_goal: thematic) if params["search"]["thematics"] == thematic
+      end
+    end
   end
 
   def dashboard
     @project = Project.find(params[:id])
     authorize @project
     @manager = manager?
+  end
+
+  def search
   end
 
   private
