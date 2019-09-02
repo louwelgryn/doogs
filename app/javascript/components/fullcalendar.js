707089b574c23d5da2195594a2a25665ae8bfe53
Marie Stoppa <marie.stoppa@essec.edu>
2019-09-02 12:17:40 +0200

    creer un evenement

--- app/javascript/components/fullcalendar.js
+++ app/javascript/components/fullcalendar.js
@@ -30,9 +30,84 @@ const calendarInit = () => {
 
       dates.forEach((day) => {
         day.addEventListener("click", (event) => {
-          console.log(day.dataset.date);
+          const selectedDay = Number.parseInt(day.dataset.date.slice(-2),10);
           $('#eventModal').modal('show');
           document.getElementById("start_date").innerHTML = day.dataset.date;
+
+          // Set start date to selected
+          const options_start_day = document.querySelectorAll("#event_start_time_3i > option");
+
+          options_start_day.forEach((option) => {
+            const dayToSelect = Number.parseInt(option.value,10);
+            if (selectedDay === dayToSelect) {
+              option.selected = true
+            } else {
+                option.selected = false
+            }
+          });
+            // Set end date to selected
+          const options_end_day = document.querySelectorAll("#event_end_time_3i > option");
+
+          options_end_day.forEach((option) => {
+            const dayToSelect = Number.parseInt(option.value,10);
+            if (selectedDay === dayToSelect) {
+              option.selected = true
+            } else {
+                option.selected = false
+            }
+          });
+
+          const selectedMonth = Number.parseInt(day.dataset.date.slice(-5,-3),10);
+
+          // Set start month to selected
+          const options_start_month = document.querySelectorAll("#event_start_time_2i > option");
+
+          options_start_month.forEach((option) => {
+            const monthToSelect = Number.parseInt(option.value,10);
+            if (selectedMonth === monthToSelect) {
+              option.selected = true
+            } else {
+                option.selected = false
+            }
+          });
+
+          // Set end month to selected
+          const options_end_month = document.querySelectorAll("#event_end_time_2i > option");
+
+          options_end_month.forEach((option) => {
+            const monthToSelect = Number.parseInt(option.value,10);
+            if (selectedMonth === monthToSelect) {
+              option.selected = true
+            } else {
+                option.selected = false
+            }
+          });
+
+          const selectedYear = Number.parseInt(day.dataset.date.slice(-10,-6),10);
+
+          // Set start year to selected
+          const options_start_year = document.querySelectorAll("#event_start_time_1i > option");
+
+          options_start_year.forEach((option) => {
+            const yearToSelect = Number.parseInt(option.value,10);
+            if (selectedYear === yearToSelect) {
+              option.selected = true
+            } else {
+                option.selected = false
+            }
+          });
+
+          // Set end month to selected
+          const options_end_year = document.querySelectorAll("#event_end_time_1i > option");
+
+          options_end_year.forEach((option) => {
+            const yearToSelect = Number.parseInt(option.value,10);
+            if (selectedYear === yearToSelect) {
+              option.selected = true
+            } else {
+                option.selected = false
+            }
+          });
           // eventLink.click();
         });
       });
