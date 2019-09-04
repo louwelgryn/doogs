ebd5cd29b380ff12c4fb91e4712a068e7ffb1309
Marie Stoppa <marie.stoppa@essec.edu>
2019-09-04 17:35:37 +0200

    circles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -139,5 +139,23 @@ $("#login").click(function() {
 });
 
 
+/// CIRCLES ///
+const displays = document.querySelectorAll('.note-display');
+const transitionDuration = 900;
+
+displays.forEach(display => {
+  let progress = display.querySelector('.circle__progress--fill');
+  let radius = progress.r.baseVal.value;
+  let circumference = 2 * Math.PI * radius;
+  let note = parseFloat(display.dataset.note);
+  let offset = circumference * (15 - note) / 15;
+
+  progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
+  progress.style.setProperty('--initialStroke', circumference);
+
+  setTimeout(() => progress.style.strokeDashoffset = offset, 100);
+});
+
+
 
 
