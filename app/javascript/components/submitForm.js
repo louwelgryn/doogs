5af67fb02fd3f6511fce819ecd0294c1c4f6ca3a
maxence ballande <maxence.ballande.16@neoma-bs.com>
2019-09-05 19:35:37 +0200

    selecttooooo okkkk

--- app/javascript/components/submitForm.js
+++ app/javascript/components/submitForm.js
@@ -1,12 +1,19 @@
 const submitForm = () => {
   const form = document.querySelector('.filter-search');
-  const formInputs = document.querySelectorAll('.filter-search select');
+  const formInputs = document.querySelectorAll('.filters');
+
+  console.log(formInputs);
 
   formInputs.forEach((input) => {
     input.addEventListener('change', (event) => {
       form.submit();
     })
   })
+
+  // selectInput = document.querySelector('.select2-selection');
+  // selectInput.addEventListener('change', (event) => {
+  //   form.submit();
+  // })
 }
 
 
